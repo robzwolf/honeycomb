8cb84dbfa17a9d20ae5d93af34ec491f201754a6
Robbie Jakob-Whitworth <mail@rjwhitworth.co.uk>
2021-04-08 14:16:16 +0100

    Trim transparent space from image

--- api/get-png.js
+++ api/get-png.js
@@ -1,4 +1,5 @@
 import * as playwright from 'playwright-aws-lambda';
+import sharp from "sharp";
 
 export default async (req, res) => {
     if (req.method !== 'POST') {
@@ -23,12 +24,15 @@ export default async (req, res) => {
     const element = await page.$('.honeycomb')
 
     // If element is not found, fallback to a full-page screenshot
-    const data = await (element || page).screenshot({
+    const imageWithSpace = await (element || page).screenshot({
         type: "png",
         omitBackground: true
     })
     await browser.close()
+
+    const imageTrimmed = await sharp(imageWithSpace).trim().toBuffer()
+
     res.setHeader("Cache-Control", "s-maxage=31536000, stale-while-revalidate")
     res.setHeader('Content-Type', 'image/png')
-    res.end(data)
+    res.end(imageTrimmed)
 }
